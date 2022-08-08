24547fafb43b5c35f5e0d2b4c805659e7f16f215
jvp119 <jayvanput@gmail.com>
2022-08-07 21:07:02 -0400

    Added entry model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Entry(models.Model):
+    puzzle_date = models.DateField()
+    solve_date = models.DateField()
+    elapsed_seconds = models.IntegerField()
+    checked = models.BooleanField()
+    revealed = models.BooleanField()
+    streak = models.BooleanField()
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ["-solve_date"]
+        verbose_name_plural = "entries"
