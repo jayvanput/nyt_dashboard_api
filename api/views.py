410544c54f5811b44340ae5f6ddc411f95c6db3d
jvp119 <jayvanput@gmail.com>
2022-08-28 22:18:12 -0400

    Added all filters for API from frontend

--- api/views.py
+++ api/views.py
@@ -1,4 +1,3 @@
-from xml.dom.minidom import ReadOnlySequentialNamedNodeMap
 from django.shortcuts import render
 from api.serializers import EntrySerializer
 from api.models import Entry
@@ -19,22 +18,21 @@ class EntryViewSet(viewsets.ModelViewSet):
             return Entry.objects.none()
         
         # Day filter
-        day = self.request.GET.get('day')
-        if day:
-            resp = resp.filter(day=day)
-        
+        days = self.request.GET.getlist('day')
+        if days:
+            resp = resp.filter(day__in=days)
+
         # Used help filter
-        used_help = self.request.GET.get('used_help')
-        if used_help:
-            resp = resp.filter(used_help=used_help)
+        used_help = self.request.GET.get('used_help') or 0
+        if not used_help:
+            resp = resp.filter(used_help=0)
 
         # Date Range filter
         start_date = self.request.GET.get('start_date')
         end_date = self.request.GET.get('end_date') or datetime.date.today().strftime("%Y-%m-%d")
-        print(start_date,end_date)
         if start_date:
-            resp = resp.filter(puzzle_date__gte=start_date,puzzle_date__lte=end_date)
-
+            resp = resp.filter(puzzle_date__range=[start_date, end_date])
+        resp = resp.order_by('-puzzle_date')
         return resp
         
 
