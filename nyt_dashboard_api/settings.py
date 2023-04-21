e2915bbeed50b3dd4de1a64f7a802213be7d363b
jvp119 <jayvanput@gmail.com>
2023-04-21 00:16:17 -0400

    Decouple settings.py from config for deployment

--- nyt_dashboard_api/settings.py
+++ nyt_dashboard_api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,13 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [config('ALLOWED_HOSTS')]
 
-CORS_ALLOWED_ORIGINS = [
-"http://localhost:3000",
-]
+CORS_TRUSTED_ORIGINS = [config('CORS_TRUSTED_ORIGINS')]
 # Application definition
 
 INSTALLED_APPS = [
@@ -121,8 +120,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
-STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
